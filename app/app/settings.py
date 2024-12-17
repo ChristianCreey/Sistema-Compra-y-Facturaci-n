1a37c2b966a1958cd30173cf4aac10cad73cc7dd
CRUZ Christian E. TENARIS <CECRUZ@tenaris.com>
2024-12-17 09:25:07 -0600

    fix: resolucion de conflictos

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bases',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
